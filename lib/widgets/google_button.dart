1a8d759120228c86f8ee62f814e6d21146e38748
Woloyor <yuralysyshak@gmail.com>
2022-11-05 21:32:59 +0200

    google auth

--- lib/widgets/google_button.dart
+++ lib/widgets/google_button.dart
@@ -1,17 +1,53 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:growy/consts/firebase_consts.dart';
 import 'package:growy/widgets/text_widget.dart';
+
+import '../screens/btm_bar.dart';
+import '../services/global_methods.dart';
 
 class GoogleButton extends StatelessWidget {
   const GoogleButton({Key? key}) : super(key: key);
+
+  Future<void> _googleSignIn(context) async {
+    final googleSignIn = GoogleSignIn();
+    final googleAccount = await googleSignIn.signIn();
+    if (googleAccount != null) {
+      final googleAuth = await googleAccount.authentication;
+      if (googleAuth.idToken != null && googleAuth.accessToken != null) {
+        try {
+          await authInstance.signInWithCredential(
+            GoogleAuthProvider.credential(
+              idToken: googleAuth.idToken,
+              accessToken: googleAuth.accessToken,
+            ),
+          );
+          Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => const BottomBarScreen()));
+        } on FirebaseException catch (error) {
+          GlobalMethods.errorDialog(
+              subtitle: '${error.message}', context: context);
+        } catch (error) {
+          GlobalMethods.errorDialog(subtitle: '$error', context: context);
+        } finally {
+          await Firebase.initializeApp();
+        }
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.blue.withOpacity(0.7),
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          _googleSignIn(context);
+        },
         child: Row(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
